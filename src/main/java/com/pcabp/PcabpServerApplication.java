c15ba4820deca84c36f4d47672101647341ff2ad
codes <codesver@gmail.com>
2025-12-17 20:47:06 +0900

    init

---
+++ src/main/java/com/pcabp/PcabpServerApplication.java
@@ -0,0 +1,13 @@
+package com.pcabp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcabpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcabpServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pcabp/PcabpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcabp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcabpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
