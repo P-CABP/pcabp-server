e03008d4e5be0797ca0e7f3f068f6a9677e3a4fc
codes <codesver@gmail.com>
2026-01-04 12:02:10 +0900

    feat(session): compose session deletion API

--- src/main/java/com/pcabp/core/user/controller/SessionController.java
+++ src/main/java/com/pcabp/core/user/controller/SessionController.java
@@ -1,13 +1,15 @@
 package com.pcabp.core.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import com.pcabp.core.user.vo.SessionUser;
 import com.pcabp.helper.SessionHolder;
 import com.pcabp.model.RestResponse;
 import com.pcabp.session.SessionUserDetails;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -17,7 +19,7 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "세션 API", description = "세션 API 입니다.")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/v1/user/session")
+@RequestMapping("/v1/session")
 public class SessionController {
 
     @Operation(summary = "세션 정보 조회", description = "세션 정보를 조회합니다.")
@@ -30,5 +32,18 @@ public class SessionController {
         }
 
         return RestResponse.ofSuccess(null);
+    }
+
+    @Operation(summary = "세션 삭제", description = "세션을 삭제합니다.")
+    @DeleteMapping
+    public RestResponse<Boolean> deleteSession(HttpServletRequest request) {
+        SecurityContextHolder.clearContext();
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+
+        return RestResponse.ofSuccess(true);
     }
 }
